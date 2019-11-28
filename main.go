3ed5f7a519cfd43ae80d0b3be979a083b108576a
Zett-8 <kame0008@gmail.com>
2019-11-29 00:07:51 +0900

    make quotes unreadable

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	var quotes []string
 
-	f, err := os.Open("s_quotes.txt")
+	f, err := os.Open("quotes.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -19,29 +19,35 @@ func main() {
 
 	sc := bufio.NewScanner(f)
 	for sc.Scan() {
-		quotes = append(quotes, en(sc.Text()))
+		quotes = append(quotes, de(sc.Text()))
 	}
 
 	rand.Seed(time.Now().UnixNano())
 	ran := int32(len(quotes))
 
 	fmt.Println(quotes[rand.Int31n(ran)])
-
-	fmt.Println(quotes)
 }
 
-func en(S string) string {
-	var n string
-	for _, s := range S {
-		n += string(s - rune(1))
-	}
-	return n
-}
+//func en(S string) string {
+//	var n string
+//	for _, s := range S {
+//		if string(s) == " " {
+//			n += " "
+//		} else {
+//			n += string(s - rune(1))
+//		}
+//	}
+//	return n
+//}
 
 func de(S string) string {
 	var n string
 	for _, s := range S {
-		n += string(s + rune(1))
+		if string(s) == " " {
+			n += " "
+		} else {
+			n += string(s + rune(1))
+		}
 	}
 	return n
 }
